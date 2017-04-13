e9b61c5ccd0ead7618708a542de41ddc6af7272d
Kris Heinrich <krisheinrich@gmail.com>
2017-04-13 11:19:23 -0700

    provide schema to GraphQL

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 // Initialize server instance
 const app = express();
 
 // Config GraphQL
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true
 }));
 
